3e1049cad8c28f93104c3a8f0682777e3ec32959
Rifala <arifatulfadia.1620@gmail.com>
2022-06-23 21:30:15 +0700

    uas

---
+++ src/Model/PokemonGo.java
@@ -0,0 +1,42 @@
+public class PokemonGo {
+    private String Bug;
+    private Integer Dark;
+    private Integer Dragon;
+    private Integer Fighting;
+
+    public String getBug() {
+        return Bug;
+    }
+
+    public void setBug(String bug) {
+        Bug = bug;
+    }
+
+    public Integer getDark() {
+        return Dark;
+    }
+
+    public void setDark(Integer dark) {
+        Dark = dark;
+    }
+
+    public Integer getDragon() {
+        return Dragon;
+    }
+
+    public void setDragon(Integer dragon) {
+        Dragon = dragon;
+    }
+
+    public Integer getFighting() {
+        return Fighting;
+    }
+
+    public void setFighting(Integer fighting) {
+        Fighting = fighting;
+    }
+
+
+
+
+}
